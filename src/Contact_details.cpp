d63aa3dac91a7ca3e46781e511c4e4ba3cbb4611
Chrinkus <seeschickrun@gmail.com>
2019-04-08 12:35:35 -0500

    Achieved submodule linkage!

--- src/Contact_details.cpp
+++ src/Contact_details.cpp
@@ -18,13 +18,19 @@ Contact_details::Contact_details(int x, int y, int w, int h)
     copy_label("Details");
     box(FL_BORDER_BOX);
     align(FL_ALIGN_TOP_LEFT);
+
+    // TEST
+    Contact c { "Chris", "Schick", "1979-02-27" };
+    c.email("chris@schick.com");
+    c.phone("(999) 555-1234");
+    load_contact(c);
 }
 
 void Contact_details::load_contact(const Contact& contact)
 {
-    first_read->value(contact.first());
-    last_read->value(contact.last());
-    birth_read->value(contact.birth());
-    phone_read->value(contact.phone());
-    email_read->value(contact.email());
+    first_read->value(contact.first().data());
+    last_read->value(contact.last().data());
+    birth_read->value(contact.birth().data());
+    phone_read->value(contact.phone().data());
+    email_read->value(contact.email().data());
 }
